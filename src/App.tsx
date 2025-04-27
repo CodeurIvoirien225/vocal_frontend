78a160a63a81a9d6bac781b6301c4b6647b7b2b7
CodeurIvoirien225 <koneelabass1@gmail.com>
2025-04-27 11:07:53 +0000

    mise à jours

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import Home from './pages/Home';
 function App() {
   return (
     <AuthProvider>
+       <Router basename="/"></Router>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
